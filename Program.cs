10901dbe9c94a189c8bfb89c9f1a54fe6b066a9d
tanelmetsar <tanel.metsar@tktk.ee>
2024-04-26 23:15:50 +0300

    add week2 code

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+Console.WriteLine("Enter your age: ");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isageNumber = Int32.TryParse(userAge out, userAgeNum);
+
+console.WriteLine($"Parse result {isageNumber}. User is {userAgeNum} years old");
+
+if (isageNumber)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram.");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age");
+}
